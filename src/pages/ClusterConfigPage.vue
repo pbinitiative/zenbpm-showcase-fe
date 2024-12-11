da397b865e6e845308d914d815a07c4ec580f3f6
Adam Klíma <adam.klima@ibpm.cz>
2024-12-11 13:54:00 +0100

    feat: cluster info page

---
+++ src/pages/ClusterConfigPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <q-page class="q-pa-md">
+    <div class="row q-col-gutter-md">
+      <div class="col-12">
+        <q-card>
+          <q-card-section>
+            <div class="text-h6">Cluster Config</div>
+          </q-card-section>
+          <q-card-section> </q-card-section>
+        </q-card>
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script setup>
+defineOptions({
+  name: "ClusterConfigPage",
+});
+</script>
